94824d25b734650f2a83e270100b018c8622167c
Soojung Kwak <gsj2464@gmail.com>
2021-05-22 16:10:23 +0900

    인증번호 생성 메소드 변경

--- SEMI-ALAM/src/main/java/com/alam/controller/Send_SMS.java
+++ SEMI-ALAM/src/main/java/com/alam/controller/Send_SMS.java
@@ -18,41 +18,26 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Send_SMS {
-	
-	public static String numberGen(int len, int dupCd) {
 
+	// 네자리 랜덤 숫자 생성
+	public static String rndNum() {
 		Random rand = new Random();
-		String numStr = ""; // 난수가 저장될 변수
+		String num = "";
 
-		for (int i = 0; i < len; i++) {
-
-			// 0~9 까지 난수 생성
-			String ran = Integer.toString(rand.nextInt(10));
-
-			if (dupCd == 1) {
-				// 중복 허용시 numStr에 append
-				numStr += ran;
-			} else if (dupCd == 2) {
-				// 중복을 허용하지 않을시 중복된 값이 있는지 검사한다
-				if (!numStr.contains(ran)) {
-					// 중복된 값이 없으면 numStr에 append
-					numStr += ran;
-				} else {
-					// 생성된 난수가 중복되면 루틴을 다시 실행한다
-					i -= 1;
-				}
-			}
+		for (int i = 0; i < 4; i++) {
+			num += Integer.toString(rand.nextInt(10));
 		}
-		return numStr;
+
+		return num;
 	}
-	
+
 	public static void sendSMS(String phoneNum, String num) {
 		String hostNameUrl = "https://sens.apigw.ntruss.com";
 		String requestUrl = "/sms/v2/services/";
 		String requestUrlType = "/messages";
-		String accessKey = "zJRIDPlJT16Xn7RM3zoh";
-		String secretKey = "KClCaPeADKZ4b6VJlsW3FlqA1jXevS3QqLrfzzCA";
-		String serviceId = "ncp:sms:kr:266345127020:semi-project";
+		String accessKey = "*";
+		String secretKey = "*";
+		String serviceId = "*";
 		String method = "POST";
 		String timestamp = Long.toString(System.currentTimeMillis());
 		requestUrl += serviceId + requestUrlType;
@@ -69,7 +54,7 @@ public class Send_SMS {
 		bodyJson.put("type", "sms");
 		bodyJson.put("contentType", "COMM");
 		bodyJson.put("countryCode", "82");
-		bodyJson.put("from", "01051641608");
+		bodyJson.put("from", "01000000000");
 		bodyJson.put("content", "hello");
 		bodyJson.put("messages", toArr);
 
